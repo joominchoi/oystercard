fc4fdf2e05c0df7b34098196b76e77756902a293
JooMin Choi <joominchoi23@gmail.com>
2021-06-02 15:59:54 +0100

    Challenge #7: added deduct method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,13 +6,28 @@ describe Oystercard do
     expect(subject.balance).to eq 0
   end
 
-  it 'can top-up oystercard' do
-    subject.top_up(5)
-    expect(subject.balance).to eq 5
+  context '#top_up' do
+
+    it 'can top-up oystercard' do
+      subject.top_up(5)
+      expect(subject.balance).to eq 5
+    end
+
+    it 'enforces maximum balance of 90' do
+      expect { subject.top_up 91 }.to raise_error("The card limit is £#{Oystercard::LIMIT}")
+    end
+
   end
 
-  it 'enforces maximum balance of 90' do
-    expect { subject.top_up 91 }.to raise_error("The card limit is £#{Oystercard::LIMIT}")
+  context '#deduct' do
+
+    it { is_expected.to respond_to(:deduct).with(1).argument }
+
+    it 'can deduct money from oystercard' do
+      subject.top_up(10)
+      expect{ subject.deduct(5) }.to change{ subject.balance }.by (-5)
+    end
+
   end
 
 end
